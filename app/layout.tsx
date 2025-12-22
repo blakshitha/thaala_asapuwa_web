146e451d328aa8efe1d57bb341b8d6f3354b22ba
Navindu Dananga <navindudananga123@gmail.com>
2025-12-22 16:41:44 +0530

    fix: updated title

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,40 +9,43 @@ import "@/styles.css";
 import { TailwindIndicator } from "@/components/ui/breakpoint-indicator";
 
 const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
+    subsets: ["latin"],
+    variable: "--font-sans",
 });
 
 const nunito = Nunito({
-  subsets: ["latin"],
-  variable: "--font-nunito",
+    subsets: ["latin"],
+    variable: "--font-nunito",
 });
 
 const lato = Lato({
-  subsets: ["latin"],
-  variable: "--font-lato",
-  weight: "400",
+    subsets: ["latin"],
+    variable: "--font-lato",
+    weight: "400",
 });
 
 export const metadata: Metadata = {
-  title: "Tina",
-  description: "Tina Cloud Starter",
+    title: "Thaala Asapuwa",
+    description: "Sri Lankan Dance Academy of Victoria",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className={cn(fontSans.variable, nunito.variable, lato.variable)}>
-      <body className="min-h-screen bg-background font-sans antialiased">
-        <VideoDialogProvider>
-          {children}
-          <VideoDialog />
-        </VideoDialogProvider>
-        <TailwindIndicator />
-      </body>
-    </html>
-  );
+    return (
+        <html
+            lang="en"
+            className={cn(fontSans.variable, nunito.variable, lato.variable)}
+        >
+            <body className="min-h-screen bg-background font-sans antialiased">
+                <VideoDialogProvider>
+                    {children}
+                    <VideoDialog />
+                </VideoDialogProvider>
+                <TailwindIndicator />
+            </body>
+        </html>
+    );
 }
